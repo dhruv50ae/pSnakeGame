942e2066ce011ca64e20ced2bb3aa192c0606ba0
dhruv50ae <linkdp2012@gmail.com>
2022-07-18 14:43:07 +0100

    Save high score to a file and read from it

--- scoreboard.py
+++ scoreboard.py
@@ -6,8 +6,9 @@ FONT = ("Arial", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.highscore = 0
         self.penup()
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.score = 0
         self.color("white")
         self.hideturtle()
@@ -29,5 +30,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+
         self.score = 0
         self.updateScoreboard()
